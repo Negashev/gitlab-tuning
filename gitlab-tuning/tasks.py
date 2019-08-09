ce8bfaf8a9fdcc5fbce4b4248cff21d73cf979dd
Negash <i@negash.ru>
2019-08-09 22:30:02 +0300

    Add more wait (not ddos gitlab)

--- gitlab-tuning/tasks.py
+++ gitlab-tuning/tasks.py
@@ -49,6 +49,7 @@ def statistic_prepare_data(project_id, git_ssh_url, changes):
         else:
             raise gitlab.exceptions.GitlabGetError
     for change in changes:
+        time.sleep(2)
         commits = project.commits.list(query_parameters={'ref_name': change['after']}, per_page=50)
         change['after'] = commits[-1].id
         exit_recursion = False
@@ -65,6 +66,7 @@ def statistic_prepare_data(project_id, git_ssh_url, changes):
             })
         # recursion
         if len(commits) > 1 and not exit_recursion:
+            time.sleep(2)
             statistic_prepare_data.send(project_id, git_ssh_url, [change])
 
 
@@ -162,10 +164,11 @@ def gitlab_user_create(user_id):
 
 @dramatiq.actor(priority=20, max_retries=3)
 def gitlab_sync_avatars(page):
+    time.sleep(2)
     connect = ldap.initialize(LDAP_URL)
     connect.set_option(ldap.OPT_REFERRALS, 0)
     connect.simple_bind_s(LDAP_USER, LDAP_PASS)
-    time.sleep(1)
+    time.sleep(2)
     for user in gl.users.list(as_list=False, page=page, per_page=CRON_AVATARS_PER_PAGE, active=True):
         ldap_user = connect.search_s(LDAP_BASE, ldap.SCOPE_SUBTREE, f'(&(objectClass={LDAP_OBJECTCLASS_USER})(mail={user.email}))',
                                             ['cn', 'mail', 'thumbnailPhoto'])
@@ -174,7 +177,7 @@ def gitlab_sync_avatars(page):
         if 'thumbnailPhoto' in ldap_user[0][1]:
             thumbnailPhoto = ldap_user[0][1]['thumbnailPhoto'][0]
             user.avatar, width, height = resize_image(thumbnailPhoto)
-            time.sleep(1)
+            time.sleep(2)
             user.save()
             print(f'set avatar ({width}x{height}) {user.email}')
     connect.unbind()
